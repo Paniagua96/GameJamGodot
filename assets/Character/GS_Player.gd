e66e853403ff170a55a0123aec4645961592cb92
Paniagua96 <44847642+Paniagua96@users.noreply.github.com>
2023-10-31 14:17:19 -0600

    Movimiento con animaciones

--- assets/Character/GS_Player.gd
+++ assets/Character/GS_Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 @onready var mesh = $Char_Anims
+@onready var animPlayer = $Char_Anims/AnimationPlayer
+
 @export var rotationSpeed = .15
 const SPEED = 5.0
+const SPEED_RUN = 7.0
 const JUMP_VELOCITY = 4.5
 
 
@@ -17,9 +20,23 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("move_forward", "move_back", "move_right", "move_left")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		var speed = 0
+		
+		if Input.is_action_pressed("Run"):
+			speed = SPEED_RUN
+			if animPlayer.current_animation != "Run":
+				animPlayer.play("Run")
+		else:
+			speed = SPEED
+			if animPlayer.current_animation != "Walk":
+				animPlayer.play("Walk")
+					
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
+		if animPlayer.current_animation != "Idle":
+			animPlayer.play("Idle")
+			
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
